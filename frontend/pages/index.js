25e251da6012c8e97c6f225d29dbd7ece2d5b945
Ryan Cavalcanti <contatodevryan@gmail.com>
2024-03-28 16:38:06 -0300

    update to the Database and division of SSR and SPA

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -51,6 +51,8 @@ export default function HomeScreen() {
         </div>
         <p>
           <a href="/auth-page-ssr">auth-page-ssr</a>
+        </p>
+        <p>
           <a href="/auth-page-static">auth-page-static</a>
         </p>
       </form>
